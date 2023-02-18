4110b71f20ba298342dec8f4d2ec4c2d622b0982
ZiogasThlem <ziogas.thlem@gmail.com>
2023-02-18 12:31:03 +0200

    Added basic project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import LogIn from './Pages/LogIn';
+import Translation from './Pages/Translation';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+
+<BrowserRouter>
+  <div className="App">
+    <nav>
+      <li><NavLink to="/">Home sweet Home</NavLink></li>
+      <li><NavLink to="/translation">Translation</NavLink></li>
+      <li><NavLink to="/profile">Profile</NavLink></li>
+    </nav>
+    <Routes>
+      <Route path="/" element={ <LogIn />} /> 
+      <Route path="/Translation" element={ <Translation />} /> 
+      <Route path="/Profile" element={ <Profile />} /> 
+    </Routes>
+  </div>
+</BrowserRouter>
+
+
   );
 }
 
