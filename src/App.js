de8613dab8b28b0cc557cb57357c681053c5d230
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-02-22 19:35:45 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StartUpPage from './pages/StartUpPage';
+import ProfilePage from './pages/ProfilePage';
+import TranslationPage from './pages/TranslationPage';
+import NavBar from './navBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path='/' element={ <StartUpPage /> } /> 
+          <Route path='/profile' element={ <ProfilePage /> } /> 
+          <Route path='/translation' element={ <TranslationPage /> } /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
