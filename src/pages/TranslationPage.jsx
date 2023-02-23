7a096a3486cfc696cbdc7863077f74b164223a17
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-02-23 17:52:53 +0200

    Add files via upload

--- src/pages/TranslationPage.jsx
+++ src/pages/TranslationPage.jsx
@@ -30,14 +30,24 @@ const TranslationPage = () => {
     // When 'submit' button clicked, take the word and traslate in images
     const onSubmit = async ({ word })  => {
         const images = []
-        const letters = /^[A-Za-z]+$/;
+        const letters = /^[A-Za-z\s]*$/
         if(word.match(letters)){
             let index = 0;
-            for(const letter of word){
+            let trimFromExtraSpaces = word.split(' ')
+                                          .filter(letter => letter !== '')
+                                          .join(' ')
+            for(const letter of trimFromExtraSpaces){
                 index++
-                images.push(<img src={"Images/individial_signs/" + letter + ".png"} 
+                if(letter === ' '){
+                    images.push(<img src={"Images/individial_signs/space.png"} 
                                  alt={letter + ".png"} 
                                  key={index}/>)
+                }
+                else{
+                   images.push(<img src={"Images/individial_signs/" + letter.toLowerCase() + ".png"} 
+                                    alt={letter + ".png"} 
+                                    key={index}/>)
+                }
             }
             setTranslateWord(images)
             const [ error, updatedUser ] = await translationAdd(user, word)
